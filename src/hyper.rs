1bcb1000e5c71de06d929ab0ea8989268edd1e9d
Rares Chiuzbaian <chiuzbaianrares20@gmail.com>
2025-01-25 12:08:30 +0200

    Improve param ranges

--- src/hyper.rs
+++ src/hyper.rs
@@ -64,12 +64,12 @@ pub fn optimize_hyperparameters(tsp: &TspLib, num_trials: usize) -> Vec<Optimiza
 
         // ACO with wider ranges
         let aco_params = AcoParams {
-            alpha: rng.gen_range(0.5..4.0),
-            beta: rng.gen_range(1.0..5.0),
-            decay: rng.gen_range(0.01..0.5),
-            q: rng.gen_range(1.0..500.0),
-            ants: rng.gen_range(50..500),
-            iterations: rng.gen_range(200..2000),
+            alpha: rng.gen_range(1.0..5.0),
+            beta: rng.gen_range(1.0..8.0),
+            decay: rng.gen_range(0.02..0.6),
+            q: rng.gen_range(10.0..600.0),
+            ants: rng.gen_range(100..600),
+            iterations: rng.gen_range(500..3000),
         };
 
         let mut aco = AntColonyOptimization::new(
@@ -92,9 +92,9 @@ pub fn optimize_hyperparameters(tsp: &TspLib, num_trials: usize) -> Vec<Optimiza
 
         // SA with wider ranges
         let sa_params = SaParams {
-            initial_temp: rng.gen_range(1000.0..50000.0),
-            final_temp: rng.gen_range(0.0001..0.1),
-            cooling_rate: rng.gen_range(0.001..0.3),
+            initial_temp: rng.gen_range(5000.0..80000.0),
+            final_temp: rng.gen_range(0.00001..0.2),
+            cooling_rate: rng.gen_range(0.0005..0.4),
         };
 
         let mut sa = SimulatedAnnealing::new(
@@ -114,9 +114,9 @@ pub fn optimize_hyperparameters(tsp: &TspLib, num_trials: usize) -> Vec<Optimiza
 
         // GA with wider ranges
         let ga_params = GaParams {
-            population_size: rng.gen_range(100..2000),
-            generations: rng.gen_range(100..5000),
-            mutation_rate: rng.gen_range(0.001..0.3),
+            population_size: rng.gen_range(200..3000),
+            generations: rng.gen_range(500..7000),
+            mutation_rate: rng.gen_range(0.001..0.4),
         };
 
         let mut ga = GeneticAlgorithm::new(
@@ -135,11 +135,11 @@ pub fn optimize_hyperparameters(tsp: &TspLib, num_trials: usize) -> Vec<Optimiza
         };
 
         let pso_params = PsoParams {
-            num_particles: rng.gen_range(50..1000),
-            iterations: rng.gen_range(200..5000),
-            cognitive_weight: rng.gen_range(0.5..4.0),
-            social_weight: rng.gen_range(0.5..4.0),
-            inertia_weight: rng.gen_range(0.1..0.9),
+            num_particles: rng.gen_range(100..2000),
+            iterations: rng.gen_range(500..7000),
+            cognitive_weight: rng.gen_range(1.0..5.0),
+            social_weight: rng.gen_range(1.0..5.0),
+            inertia_weight: rng.gen_range(0.05..0.95),
         };
 
         let mut pso = ParticleSwarmOptimization::new(
